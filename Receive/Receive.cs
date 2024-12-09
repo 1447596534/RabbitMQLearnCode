f8fc4ca1bd8335b90483862684dd078a11499bed
1447596534 <1447596534@qq.com>
2024-12-09 23:02:53 +0800

    添加项目文件。

---
+++ Receive/Receive.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, RabbitMQ!");
+
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+var connection = await factory.CreateConnectionAsync();
+var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false); // qos调度，当前通道队列每个消费者同时最多只分配1个任务
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[X] Received {message}");
+
+    int dots = message.Split('.').Length - 1;
+    await Task.Delay(dots * 1000);
+
+    Console.WriteLine($"[X] Done");
+
+    await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);  // 确保当前在没处理完时出现中止会自动转交给别的消费者
+};
+
+await channel.BasicConsumeAsync("task_queue", autoAck: false, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ Send/Send.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+decimal.TryParse("asd", out decimal parameterxf1);
+
+
+var factory = new ConnectionFactory { HostName ="localhost" };
+var connection = await factory.CreateConnectionAsync();
+var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "task_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+
+string message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
+Console.WriteLine($" [x] Send {message}");
+Console.ReadLine();
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+}
